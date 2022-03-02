bb6c86a8ec7b35913c7e58887cc0dbbf1dd19fcf
Rochelle Longoria <rochellelong95@gmail.com>
2022-03-02 01:33:08 -0700

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var numOne = document.querySelector(".numOne");
+var sumOne = 0
+function plusCountOne() {
+sumOne++
+
+numOne.innerText= sumOne +" Like(s)"
+
+}
+
+var numTwo = document.querySelector(".numTwo");
+var sumTwo = 0
+function plusCountTwo() {
+sumTwo++
+
+numTwo.innerText= sumTwo +" Like(s)"
+
+}
+
+var numThree = document.querySelector(".numThree");
+var sumThree = 0
+function plusCountThree() {
+sumThree++
+
+numThree.innerText= sumThree +" Like(s)"
+
+}
